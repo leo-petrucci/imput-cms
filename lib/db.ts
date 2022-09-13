80b96ba7a2836cf6acc2fae1d04a8d3461aae19b
creativiii <leonardo@creativiii.com>
2022-09-13 12:42:45 +0000

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,16 +1,11 @@
 import initSqlJs from "sql.js";
-import wasmModule from 'https://sql.js.org/dist/sql-wasm.wasm?module';
 
 const initDb = async () => {
-
-  const { exports } = (await WebAssembly.instantiate(wasmModule)) as any;
-  
   // Create a database
-  const db = new SQL.Database();
+  // const db = new SQL.Database();
   // NOTE: You can also use new SQL.Database(data) where
   // data is an Uint8Array representing an SQLite database file
-
-  return db;
+  // return db;
 };
 
 export default initDb;

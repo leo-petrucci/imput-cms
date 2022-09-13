d9a59e47d11a1f28eb7a83847efb427e5e269ea5
creativiii <leonardo@creativiii.com>
2022-09-13 13:12:42 +0000

    Update hello.ts

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -2,7 +2,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 // import initDb from "../../lib/db";
 // @ts-ignore
-import wasmModule from "../../lib/sql-wasm.wasm";
+import wasmModule from "../../lib/sql-wasm.wasm?module";
 
 type Data = {
   info: any;

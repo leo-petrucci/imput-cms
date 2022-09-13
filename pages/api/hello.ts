72dfa5f72d4e0f8ef77b42502f1e7950f0a693a1
creativiii <leonardo@creativiii.com>
2022-09-13 13:14:19 +0000

    Update 2 files

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -2,23 +2,38 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 // import initDb from "../../lib/db";
 // @ts-ignore
-import wasmModule from "../../lib/sql-wasm.wasm?module";
-
-type Data = {
-  info: any;
-};
+// import wasmModule from "../../lib/sql-wasm.wasm?module";
+import wasm from "../../lib/pkg/wasm_bg.wasm?module";
 
 export const config = {
   runtime: "experimental-edge",
 };
 
-const hello = async (_req: NextApiRequest, res: NextApiResponse<Data>) => {
-  const { exports } = (await WebAssembly.instantiate(wasmModule)) as any;
-  // const db = await initDb();
-  // const stmt = db.prepare("SELECT rowid AS id, info FROM lorem");
+function convertToNumber(given: string) {
+  if (given.startsWith("0x")) {
+    return parseInt(given.slice(2), 16);
+  }
 
-  // const result = stmt.getAsObject();
-  res.status(200).json({ info: exports });
-};
+  return parseInt(given, 10);
+}
 
-export default hello;
+function hexFormat(given: number) {
+  const str = given.toString(16);
+  return "0x" + "0".repeat(8 - str.length) + str;
+}
+
+export default async function handler(request: Request, event: Event) {
+  const url = new URL(request.url);
+
+  if (!url.searchParams.get("a") || !url.searchParams.get("b")) {
+    return new Response("Two inputs are required", { status: 400 });
+  }
+
+  const a = convertToNumber(url.searchParams.get("a")!);
+  const b = convertToNumber(url.searchParams.get("b")!);
+
+  const { exports } = (await WebAssembly.instantiate(wasm)) as any;
+
+  const value = exports.xor(a, b);
+  return new Response(hexFormat(value));
+}

7731b650c583089fe317ff92f041d94859d42c09
Leonardo Petrucci <leo.petrucci@rvu.co.uk>
2023-02-07 17:42:02 +0000

    Possible build fix

--- examples/cms/next.config.js
+++ examples/cms/next.config.js
@@ -3,24 +3,6 @@ const path = require('path')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    externalDir: true,
-  },
-  webpack: (
-    config,
-    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-  ) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      cms: path.resolve(__dirname, '../../src/cms/'),
-      'stitches.config$': path.resolve(__dirname, '../../stitches.config.ts'),
-      'node_modules/modern-normalize/modern-normalize.css$': path.resolve(
-        __dirname,
-        '../../node_modules/modern-normalize/modern-normalize.css'
-      ),
-    }
-    return config
-  },
 }
 
 module.exports = nextConfig

02eb7e263ccab65adc2622a80d5a3f23b138d41c
Leonardo Petrucci <leo.petrucci@rvu.co.uk>
2023-03-12 19:06:45 +0000

    Fix props

--- examples/docs/theme.config.jsx
+++ examples/docs/theme.config.jsx
@@ -6,11 +6,7 @@ export default {
     link: 'https://github.com/shuding/nextra',
   },
   components: {
-    Note: ({
-      children: {
-        props: { children },
-      },
-    }) => {
+    Note: ({ children }) => {
       return (
         <div className="nextra-callout nx-overflow-x-auto nx-mt-6 nx-flex nx-rounded-lg nx-border nx-py-2 ltr:nx-pr-4 rtl:nx-pl-4 contrast-more:nx-border-current contrast-more:dark:nx-border-current nx-border-orange-100 nx-bg-orange-50 nx-text-orange-800 dark:nx-border-orange-400/30 dark:nx-bg-orange-400/20 dark:nx-text-orange-300">
           <div className="nx-select-none nx-text-xl ltr:nx-pl-3 ltr:nx-pr-2 rtl:nx-pr-3 rtl:nx-pl-2">

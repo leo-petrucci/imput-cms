36b0e6ab27945dbe96f82b1d334bf164c5af7d4d
Leonardo Petrucci <leo.petrucci@rvu.co.uk>
2024-04-01 15:47:20 +0100

    Woops

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -11,15 +11,16 @@ export const revalidate = 60 // revalidate this page every 60 seconds
 const Page = () => {
   const { data, isLoading } = useSWR('/content/homepage.mdx', async (arg) => {
     const mdx = await axios.get(arg)
-    return await serialize(mdx.data, {
+    const serialized = serialize(mdx.data, {
       parseFrontmatter: true,
       mdxOptions: {
         development: process.env.NODE_ENV === 'development',
       },
     })
+    return serialized
   })
   if (isLoading) return <>Loading...</>
-  return <MDXRemote {...data} components={Components} />
+  return <MDXRemote {...data!} components={Components} />
 }
 
 export default Page

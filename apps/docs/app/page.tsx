ebc800e1b497bc35d318a9123dea31bd44329816
Leonardo Petrucci <leo.petrucci@rvu.co.uk>
2024-04-03 21:18:22 +0100

    Why

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -4,31 +4,31 @@ import { MDXRemote } from '@/components/MDXRemote/MDXRemote'
 import { config } from '@/docs.config'
 import { Metadata } from 'next'
 
-export async function generateMetadata(): Promise<Metadata> {
-  const mdx = await axios.get(`${config.baseUrl}/content/homepage.mdx`)
-  const serialized = await serialize<
-    any,
-    {
-      title: string
-      description: string
-    }
-  >(mdx.data, {
-    parseFrontmatter: true,
-    mdxOptions: {
-      development: process.env.NODE_ENV === 'development',
-    },
-  })
+// export async function generateMetadata(): Promise<Metadata> {
+//   const mdx = await axios.get(`${config.baseUrl}/content/homepage.mdx`)
+//   const serialized = await serialize<
+//     any,
+//     {
+//       title: string
+//       description: string
+//     }
+//   >(mdx.data, {
+//     parseFrontmatter: true,
+//     mdxOptions: {
+//       development: process.env.NODE_ENV === 'development',
+//     },
+//   })
 
-  return {
-    title: serialized.frontmatter.title,
-    description: serialized.frontmatter.description,
-    openGraph: {
-      title: serialized.frontmatter.title,
-      description: serialized.frontmatter.description,
-      images: ['/homepage-open-graph.png'],
-    },
-  }
-}
+//   return {
+//     title: serialized.frontmatter.title,
+//     description: serialized.frontmatter.description,
+//     openGraph: {
+//       title: serialized.frontmatter.title,
+//       description: serialized.frontmatter.description,
+//       images: ['/homepage-open-graph.png'],
+//     },
+//   }
+// }
 
 export const revalidate = 60 // revalidate this page every 60 seconds
 

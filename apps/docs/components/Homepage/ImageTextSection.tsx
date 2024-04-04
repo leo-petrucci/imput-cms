f2f0217f3cd670951229886298c77fd2282b790a
Leonardo Petrucci <leo.petrucci@rvu.co.uk>
2024-04-04 08:42:21 +0100

    🐛 Update homepage

--- apps/docs/components/Homepage/ImageTextSection.tsx
+++ apps/docs/components/Homepage/ImageTextSection.tsx
@@ -1,4 +1,4 @@
-import { FakeP, H2, H3 } from '@imput/components/Typography'
+import { FakeP, H3 } from '@imput/components/Typography'
 
 type ImageTextSectionProps = {
   title: string
@@ -16,13 +16,13 @@ const ImageTextSection = ({
   return (
     <div className="relative flex justify-center h-64 items-stretch border-b border-border overflow-hidden">
       <div
-        className={`relative flex flex-col justify-center max-w-5xl w-full gap-2 bg-no-repeat ${direction === 'ltr' ? `bg-right` : `bg-left`}`}
+        className={`@container relative flex flex-col justify-center max-w-5xl w-full gap-2 bg-no-repeat ${direction === 'ltr' ? `bg-right` : `bg-left`}`}
         style={{
           backgroundImage: `url(${imageSrc})`,
         }}
       >
         <div
-          className={`@container flex flex-col justify-center py-4 px-4 @md:px-0 max-w-[100%] @md:max-w-[50%] flex-1 w-full ${direction === 'ltr' ? `self-start` : `self-end`} bg-white/90 @md:bg-transparent`}
+          className={` flex flex-col justify-center py-4 px-4 @3xl:px-0 max-w-[100%] @3xl:max-w-[50%] flex-1 w-full ${direction === 'ltr' ? `self-start` : `self-end`} bg-white/50 @3xl:bg-transparent`}
         >
           <H3 className="text-foreground">{title}</H3>
           <FakeP className="text-slate-700">{children}</FakeP>

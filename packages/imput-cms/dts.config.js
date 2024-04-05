a149c74e7cbd224e7d54a03cd2b2575f708e1dbc
Leonardo Petrucci <leonardo@creativiii.com>
2024-04-05 17:39:39 +0100

    fix: 🐛 Switch build system for something better (#117)
    * fix: 🐛 Switch build system for something better
    
    * Update lockfile
    
    * Add missing dep
    
    * Forgot to minify

--- packages/imput-cms/dts.config.js
+++
@@ -1,8 +0,0 @@
-const css = require('rollup-plugin-import-css')
-
-module.exports = {
-  rollup(config, options) {
-    config.plugins?.push(css())
-    return config // always return a config.
-  },
-}

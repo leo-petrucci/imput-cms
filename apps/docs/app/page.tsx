8bc2a222753ccb8f67e2f3778f9873536e74a0a2
Leonardo Petrucci <leo.petrucci@rvu.co.uk>
2024-09-07 21:37:57 +0100

    🐛 Make homepage static

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,19 +1,21 @@
-import axios from 'axios'
 import { serialize } from 'next-mdx-remote/serialize'
 import { MDXRemote } from '@/components/MDXRemote/MDXRemote'
-import { config } from '@/docs.config'
 import { Metadata } from 'next'
+import fs from 'fs/promises'
 
 export async function generateMetadata(): Promise<Metadata> {
   try {
-    const mdx = await axios.get(`${config.baseUrl}/content/homepage.mdx`)
+    const mdx = await fs.readFile(
+      process.cwd() + '/public/content/homepage.mdx',
+      'utf-8'
+    )
     const serialized = await serialize<
       any,
       {
         title: string
         description: string
       }
-    >(mdx.data, {
+    >(mdx, {
       parseFrontmatter: true,
       mdxOptions: {
         development: process.env.NODE_ENV === 'development',
@@ -38,9 +40,12 @@ export async function generateMetadata(): Promise<Metadata> {
 
 const Page = async () => {
   try {
-    const mdx = await axios.get(`${config.baseUrl}/content/homepage.mdx`)
+    const mdx = await fs.readFile(
+      process.cwd() + '/public/content/homepage.mdx',
+      'utf-8'
+    )
 
-    return <MDXRemote source={mdx.data.split('---')[2]} />
+    return <MDXRemote source={mdx.split('---')[2]} />
   } catch (err) {
     console.log('Error rendering homepage', err)
     return <>test</>
